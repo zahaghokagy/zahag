90c9363915a69576fe12ab8f4ee56a0bc58c8e6a
zahaghokagy <crea.arab2@gmail.com>
2019-09-11 23:00:39 +0100

    update new

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -13,13 +13,17 @@ const Footer = () => {
           or you can find me on{" "}
           <a href="https://www.upwork.com" target="_blank">
             Upwork
+          </a>{" "}
+          or{" "}
+          <a href="https://www.github.com/zahaghokagy" id="git" target="_blank">
+            Github
           </a>
         </h2>
       </div>
       <hr id="hr" className="m-auto" />
-      <div className="row m-auto justify-content-center p-3 foot">
-        Copyright ©{new Date().getFullYear() + ""} <b>&nbsp;Zahag Design</b>.
-        Illustration by
+      <div className="row m-auto justify-content-center py-3 foot">
+        Copyright ©{new Date().getFullYear() + ""} <b>&nbsp;Zahag Design</b>
+        &nbsp;| Illustration by
         <a href="https://www.freepik.com/pikisuperstar" target="_blank">
           <img src={freepick} width="60px" className="mx-2 " alt="" />
         </a>
